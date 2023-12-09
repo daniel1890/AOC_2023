da7bf5b21e9b4511c478186c064df8340244ae92
Daniel Roth <d.roth@student.han.nl>
2023-12-09 10:54:29 +0100

    finish day 2, maybe will refactor sometime in the future

--- src/main/java/d/roth/day2/Day2.java
+++ src/main/java/d/roth/day2/Day2.java
@@ -55,6 +55,42 @@ public class Day2 extends Day {
 
     @Override
     protected int calcPartTwo() {
-        return 0;
+        return input.stream().map(
+                (String line) -> {
+                    // setup new hashmap for colors and minimum value
+                    HashMap<String, Integer> valueColorMap = new HashMap<>() {{
+                        put("red", 0);
+                        put("green", 0);
+                        put("blue", 0);
+                    }};
+
+                    // parse game
+                    String[] splitLineOnIDArr = line.split(":");
+
+                    // split game sets
+                    String[] gameSets = splitLineOnIDArr[1].replaceAll("\\s+", " ").split(";");
+
+                    // loop though all game sets
+                    for(String gameSet : gameSets) {
+                        String[] games = gameSet.split(",");
+
+                        // loop through all games
+                        for(String game: games) {
+                            String[] gameValues = game.split(" ");
+                            int gameValue = Integer.parseInt(gameValues[1]);
+                            String gameColorKey = gameValues[2];
+
+                            // compare if value of color key is lower, if value is lower put new higher value
+                            if(valueColorMap.containsKey(gameColorKey)) {
+                                if(valueColorMap.get(gameColorKey) < gameValue) {
+                                    valueColorMap.put(gameColorKey, gameValue);
+                                }
+                            }
+                        }
+                    }
+
+                    // return power of highest values for each color per game, of these values per game return the sum of these values for the answer
+                    return valueColorMap.values().stream().mapToInt(value -> value).reduce(1, (a, b) -> a * b);
+                }).mapToInt(Integer::intValue).sum();
     }
 }
